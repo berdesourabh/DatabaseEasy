644fc6b4f98969de2a001d9817956a5069e75999
Sourabh Berde <sourabh.berde@emc.com>
2018-05-01 16:33:40 +0530

    Removed Unwanted Component and added new functionality

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,10 +11,12 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/view/Login.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/view/Main.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("DatabaseEasy");
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -3,42 +3,129 @@ package controller;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 
 public class MainController implements Initializable {
+
+	Connection connection;
+
+	DatabaseMetaData metaData;
+	@FXML
+	public TextField txtUsername;
+
+	@FXML
+	public TextField txtPassword;
+
+	@FXML
+	public TextField txtDatabase;
+
+	@FXML
+	public TextField txtServerUrl;
+
+	@FXML
+	public Label lblError;
+
 	@FXML
 	public Label lblMainDBName;
 
 	@FXML
+	public Label lblTableName;
+
+	@FXML
 	public ListView<String> listTableNames;
+
+	@FXML
+	public ListView<String> listColumnNames;
+	
+	@FXML
+	public GridPane createPane;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
 
 	}
 
-	public void showTables(Connection con, String dbName) throws SQLException {
-		DatabaseMetaData md = con.getMetaData();
+	public void clearContent() {
+		lblError.setText("");
+		listTableNames.getItems().clear();
+		listColumnNames.getItems().clear();
+		lblTableName.setText("");
+	}
+
+	@FXML
+	public void onLoginClick(ActionEvent event) {
+		clearContent();
+		try {
+			Class.forName("org.postgresql.Driver");
+			connection = DriverManager.getConnection(
+					"jdbc:postgresql://" + txtServerUrl.getText() + ":5432/" + txtDatabase.getText(),
+					txtUsername.getText(), txtPassword.getText());
+			if (connection.isValid(0)) {
+				showTables(txtDatabase.getText());
+			}
+		} catch (
+
+		Exception e) {
+			lblError.setText(e.getLocalizedMessage());
+		}
+
+	}
+
+	public void showTables(String dbName) throws SQLException {
+		metaData = connection.getMetaData();
 		String[] types = { "TABLE" };
-		ResultSet rs = md.getTables(null, "public", "%", types);
-		List<String> tables = new ArrayList<>();
+		ResultSet rs = metaData.getTables(null, "public", "%", types);
+		ObservableList<String> tableList = FXCollections.observableArrayList();
 
 		while (rs.next()) {
-			tables.add(rs.getString(3));
+			tableList.add(rs.getString(3));
 		}
-		listTableNames.getItems().addAll(tables);
+		listTableNames.getItems().addAll(tableList);
 		lblMainDBName.setText(dbName);
 
 	}
 
+	public void showColumns(String selectedTable) {
+		try {
+			lblTableName.setText(selectedTable.toUpperCase());
+			listColumnNames.getItems().clear();
+			ResultSet columnResultSet = metaData.getColumns(null, null, selectedTable, null);
+			ObservableList<String> columnList = FXCollections.observableArrayList();
+
+			while (columnResultSet.next()) {
+				columnList.add(columnResultSet.getString("COLUMN_NAME"));
+			}
+			
+			listColumnNames.getItems().addAll(columnList);
+
+		} catch (Exception e) {
+			lblError.setText(e.getMessage());
+		}
+
+	}
+
+	@FXML
+	public void handleMouseClick(MouseEvent arg0) {
+		try {
+			showColumns(listTableNames.getSelectionModel().getSelectedItem());
+		} catch (Exception e) {
+			lblError.setText(e.getMessage());
+		}
+
+	}
 }
